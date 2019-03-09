4b21385ca2ffe74f4f78baecc78fcae6dfddfdb0
Pilchowie <m.sadowski@opoczta.pl>
2019-03-09 13:31:14 +0100

    HelloWorld3.java

---
+++ src/pl/testerkrk3/Loops.java
@@ -0,0 +1,39 @@
+package pl.testerkrk3;
+
+import java.sql.SQLOutput;
+
+public class Loops {
+    public static void main(String[] args) {
+        int[] liczby = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+
+//        for (int i = 0; i < liczby.length; i++) {
+//            if (liczby[i] % 2 == 0) {
+//                System.out.println(liczby[i]);
+//            }
+
+
+//        for (int liczba : liczby) {
+//            if (liczba % 2 == 0) {
+//                System.out.println(liczba);
+//            }
+        int i = 0;
+//        while (i < liczby.length) {
+//            if (liczby[i] % 2 == 0) {
+//                System.out.println(liczby[i]);
+//            }
+//            i++;
+//        }
+        do {
+            if (liczby[i] % 2 == 0) {
+                System.out.println(liczby[i]);
+            }
+            i++;
+
+        } while (i < liczby.length) ;
+
+    }
+}
+
+
+
+
